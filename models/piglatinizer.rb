9cabc523efb1d022c6561d0335390456351f8607
Daniel Sasser <danielbsasser@gmail.com>
2020-03-20 01:31:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
   def convert_to_pig_latin
     split_input = @text.split(" ")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    split_input.map |word|
+    split_input.map do |word|
       if vowels.include?(word[0])
         word = word + "way"
       else
@@ -16,4 +16,5 @@ class PigLatinizer
     end
     split_input.join(" ")
   end
+  
 end
