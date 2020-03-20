eb9c1d60465fe19c0a7861eda353e4986ce3e806
Daniel Sasser <danielbsasser@gmail.com>
2020-03-20 01:38:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @converted_text = PigLatinizer.new(params[:user_text])
+    @converted_text = params[:user_text]
     erb :piglatinize
   end
 
