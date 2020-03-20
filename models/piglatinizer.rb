6a792e9c048b48e5785db8af6f9655b6914fe718
Daniel Sasser <danielbsasser@gmail.com>
2020-03-20 00:56:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,18 +2,18 @@ class PigLatinizer
 
   def initialize(text)
     @input = text
-  end 
+  end
 
   def self.convert_to_pig_latin
     split_input = @input.split(" ")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     split_input.each do |word|
-      if vowels.include?(word.first) 
+      if vowels.include?(word.first)
         word = word + "way"
-      else 
+      else
         word = word.substring(1) + word.first + "ay"
       end
-    end 
+    end
     split_input.join(" ")
   end
 end
